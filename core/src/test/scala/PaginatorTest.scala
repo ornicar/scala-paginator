f51054bddf2b2b7908173947ccc771471a9bda38
Thibault Duplessis <thibault.duplessis@gmail.com>
2011-11-23 00:27:40 +0100

    Update tests to new paginator constructor signature

--- core/src/test/scala/PaginatorTest.scala
+++ core/src/test/scala/PaginatorTest.scala
@@ -8,40 +8,46 @@ class PaginatorTest extends FunSuite {
 
   test("Empty seq") {
     val a = makeAdapter[Int]()
-    val p = new Paginator(a, 1, 10)
-    assert(p.currentPage === 1)
-    assert(p.maxPerPage === 10)
-    assert(p.nbResults === 0)
-    assert(p.currentPageResults === Seq())
-    assert(p.nbPages === 0)
+    Paginator(a, 1, 10).fold( { f => fail(f) }, { p =>
+      assert(p.currentPage === 1)
+      assert(p.maxPerPage === 10)
+      assert(p.nbResults === 0)
+      assert(p.currentPageResults === Seq())
+      assert(p.nbPages === 0)
+    })
   }
 
   test("Big seq") {
     val a = makeAdapter('a' to 'z')
-    val p = new Paginator(a, 1, 10)
-    assert(p.currentPageResults === ('a' to 'j'))
-    assert(p.nbResults === 26)
-    assert(p.nbPages === 3)
+    Paginator(a, 1, 10).fold( { f => fail(f) }, { p =>
+      assert(p.currentPageResults === ('a' to 'j'))
+      assert(p.nbResults === 26)
+      assert(p.nbPages === 3)
+    })
   }
 
   test("Previous page") {
     val a = makeAdapter('a' to 'z')
-    val p1 = new Paginator(a, 1, 10)
-    assert(p1.previousPage === None)
-    assert(p1.hasPreviousPage === false)
-    val p2 = new Paginator(a, 2, 10)
-    assert(p2.previousPage === Option(1))
-    assert(p2.hasPreviousPage === true)
+    Paginator(a, 1, 10).fold( { f => fail(f) }, { p =>
+      assert(p.previousPage === None)
+      assert(p.hasPreviousPage === false)
+    })
+    Paginator(a, 2, 10).fold( { f => fail(f) }, { p =>
+      assert(p.previousPage === Option(1))
+      assert(p.hasPreviousPage === true)
+    })
   }
 
   test("Next page") {
     val a = makeAdapter('a' to 'z')
-    val p1 = new Paginator(a, 3, 10)
-    assert(p1.nextPage === None)
-    assert(p1.hasNextPage === false)
-    val p2 = new Paginator(a, 2, 10)
-    assert(p2.nextPage === Option(3))
-    assert(p2.hasNextPage === true)
+    Paginator(a, 3, 10).fold( { f => fail(f) }, { p =>
+      assert(p.nextPage === None)
+      assert(p.hasNextPage === false)
+    })
+    Paginator(a, 2, 10).fold( { f => fail(f) }, { p =>
+      assert(p.nextPage === Option(3))
+      assert(p.hasNextPage === true)
+    })
   }
 
   private def makeAdapter[A](s: Seq[A] = Seq()) =
