0eee3055051d190e116ea57abe91361b70300609
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-01-21 18:02:35 +0100

    Bump version 1.2

--- project/PaginatorBuild.scala
+++ project/PaginatorBuild.scala
@@ -6,13 +6,19 @@ object PaginatorBuild extends Build
   lazy val core = Project("core", file("core")) settings(
     organization:= "com.github.ornicar",
     name := "paginator-core",
-    version := "1.1",
+    version := "1.2",
     scalaVersion := "2.9.1",
     libraryDependencies ++= Seq(
       "org.scala-tools.testing" %% "scalacheck" % "1.9",
       "org.scala-tools.testing" % "test-interface" % "0.5",
       "org.scalatest" %% "scalatest" % "1.6.1"
-    )
+    ),
+    scalacOptions += "-deprecation",
+    scalacOptions += "-unchecked",
+    publishTo := Some(Resolver.sftp(
+      "iliaz",
+      "scala.iliaz.com"
+    ) as ("scala_iliaz_com", Path.userHome / ".ssh" / "id_rsa"))
   )
 
   lazy val salatAdapter = Project("salat-adapter", file("salat-adapter")) dependsOn(core) settings(
@@ -29,9 +35,4 @@ object PaginatorBuild extends Build
       "repo.novus snaps" at "http://repo.novus.com/snapshots/"
     )
   )
-
-  // append -deprecation to the options passed to the Scala compiler
-  scalacOptions += "-deprecation"
-
-  scalacOptions += "-unchecked"
 }
