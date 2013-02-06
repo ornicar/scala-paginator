06de25ce95218ff9bfffd249f2b4965ca7bcf593
Fabien Pennequin <fabien@pennequin.me>
2013-02-06 20:27:05 +0100

    Compatibility with scala 2.10

--- project/PaginatorBuild.scala
+++ project/PaginatorBuild.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object BuildSettings {
   val buildOrganization = "com.github.ornicar"
-  val buildScalaVersion = "2.9.1"
+  val buildScalaVersion = "2.10.0"
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := buildOrganization,
@@ -16,6 +16,7 @@ object BuildSettings {
 object Resolvers {
   val novus = "repo.novus" at "http://repo.novus.com/snapshots/"
   val sonatype = "sonatype" at "http://oss.sonatype.org/content/repositories/releases"
+  val sonatypeSnapshots = "sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots"
   val typesafe = "typesafe.com" at "http://repo.typesafe.com/typesafe/releases/"
   val iliaz = "iliaz.com" at "http://scala.iliaz.com/"
   val iliazPublish = Some(Resolver.sftp(
@@ -25,11 +26,12 @@ object Resolvers {
 }
 
 object Dependencies {
-  val scalacheck = "org.scala-tools.testing" %% "scalacheck" % "1.9" % "test"
+  val scalacompiler = "org.scala-lang" % "scala-compiler" % "2.10.0" % "optional"
+  val scalacheck = "org.scalacheck" %% "scalacheck" % "1.10.0" % "test"
   val test = "org.scala-tools.testing" % "test-interface" % "0.5" % "test"
-  val scalatest = "org.scalatest" %% "scalatest" % "1.6.1" % "test"
+  val scalatest = "org.scalatest" %% "scalatest" % "1.9" % "test"
 
-  val salat = "com.novus" %% "salat-core" % "1.9-SNAPSHOT"
+  val salat = "com.novus" %% "salat-core" % "1.9.2-SNAPSHOT"
 
   val jdubext = "com.github.ornicar" %% "jdubext" % "1.1"
 }
@@ -44,13 +46,13 @@ object PaginatorBuild extends Build {
       name := "paginator-core",
       version := "1.6",
       publishTo := iliazPublish,
-      libraryDependencies := Seq(scalacheck, test, scalatest)))
+      libraryDependencies := Seq(scalacompiler, scalacheck, test, scalatest)))
 
   lazy val salatAdapter = Project("salat-adapter", file("salat-adapter"),
     settings = buildSettings ++ Seq(
       name := "paginator-salat-adapter",
-      version := "1.4",
-      resolvers := Seq(novus, sonatype),
+      version := "1.5",
+      resolvers := Seq(novus, sonatypeSnapshots),
       publishTo := iliazPublish,
       libraryDependencies ++= Seq(salat))) dependsOn core
 
